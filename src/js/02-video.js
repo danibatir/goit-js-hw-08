99845764ab15f5901078fac48740b573c8db587c
danibatir <danibatir@gmail.com>
2024-05-02 17:49:11 +0300

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import { Player } from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const player = new Player('vimeo-player');
 
+player.on('ready', () => {
+  player.on('timeupdate', () => {
+    localStorage.setItem('videoplayer-current-time', player.getCurrentTime());
+  });
+
+  const throttledTimeupdate = throttle(() => {
+    localStorage.setItem('videoplayer-current-time', player.getCurrentTime());
+  }, 1000);
+
+  player.on('timeupdate', throttledTimeupdate);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime !== null) {
+    player.setCurrentTime(savedTime);
+  }
+});
